3fef55d39e6fc778f9228151baa81a4bedbc5716
yuxiaohong <1351130147@qq.com>
2023-09-27 18:56:56 +0800

    笔记本2023.9.27提交

---
+++ _10_Collection/src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * @author yuxiaohong
+ * @package
+ * @date 2023/9/27 11:10
+ * @description ${DESCRIPTION}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+}
---
+++ _10_Collection/src/com/learning01/collection/CollectionTest.java
@@ -0,0 +1,10 @@
+package com.learning01.collection;
+
+/**
+ * @author yuxiaohong
+ * @package com.learning01.collection
+ * @date 2023/9/27 18:56
+ * @description
+ */
+public class CollectionTest {
+}
