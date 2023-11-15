6112c78b3e38d955576de72d68e35e3af233a7ae
yuxiaohong <1351130147@qq.com>
2023-11-15 09:24:12 +0800

    笔记本2023.11.15第一次提交

---
+++ _13_io/src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * @author yuxiaohong
+ * @package
+ * @date 2023/11/9 18:39
+ * @description ${DESCRIPTION}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
