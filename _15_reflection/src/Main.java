821c69ba35ef6f7ddadbce9aca6d8dc50672e844
yuxiaohong <1351130147@qq.com>
2023-11-22 18:39:02 +0800

    笔记本2023.11.22第一次提交

---
+++ _15_reflection/src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * @author yuxiaohong
+ * @package
+ * @date 2023/11/22 18:25
+ * @description ${DESCRIPTION}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ _15_reflection/src/com/geo/learning01/example/ReflectionTest.java
@@ -0,0 +1,16 @@
+package com.geo.learning01.example;
+
+import org.junit.Test;
+
+/**
+ * @author yuxiaohong
+ * @package com.geo.learning01.example
+ * @date 2023/11/22 18:35
+ * @description
+ */
+public class ReflectionTest {
+    @Test
+    public void test() {
+
+    }
+}
