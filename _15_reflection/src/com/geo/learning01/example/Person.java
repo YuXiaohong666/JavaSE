74886c510c524776c9058fcca4aab47cb371d314
yuxiaohong <1351130147@qq.com>
2023-11-23 19:05:55 +0800

    笔记本2023.11.23第一次提交

---
+++ _15_reflection/src/com/geo/learning01/example/Person.java
@@ -0,0 +1,48 @@
+package com.geo.learning01.example;
+
+/**
+ * @author yuxiaohong
+ * @package com.geo.learning01.example
+ * @date 2023/11/23 16:54
+ * @description
+ */
+public class Person {
+    public String name;
+    public int age;
+
+    public Person() {
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void show() {
+        System.out.println("展示");
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
--- _15_reflection/src/com/geo/learning01/example/ReflectionTest.java
+++ _15_reflection/src/com/geo/learning01/example/ReflectionTest.java
@@ -2,6 +2,9 @@ package com.geo.learning01.example;
 
 import org.junit.Test;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 /**
  * @author yuxiaohong
  * @package com.geo.learning01.example
@@ -9,8 +12,32 @@ import org.junit.Test;
  * @description
  */
 public class ReflectionTest {
+
+    // 之前使用的创建对象的方法
     @Test
     public void test() {
-
+        Person tom = new Person("Tom", 23);
+        System.out.println(tom.age);
+        tom.show();
     }
+
+    // 使用反射实现
+    @Test
+    public void test2() throws Exception {
+        // 1.创建Person类的实例
+        Class<Person> person = Person.class;
+        Person person1 = person.newInstance();
+        System.out.println(person1);
+
+        // 2.调用属性
+        Field ageField = person.getField("age");
+        ageField.set(person1, 10);
+        System.out.println(ageField.get(person1));
+
+        // 3.调用方法
+        Method showMethod = person.getMethod("show");
+        showMethod.invoke(person1);
+    }
+
+    /*出了Person类之后*/
 }
